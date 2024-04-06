d5bb58196ac6545a6f8c8ccf6839b368cad84d36
Dayane <dayanebmartins@gmail.com>
2024-04-06 20:51:37 -0300

    fix(Customer):
    Insere "onDelete(Cascade)"  nas tabelas necessarias para excluir um cliente e as vendas associadas a ele

---
+++ database/migrations/1712434681023_create_phones_table.ts
@@ -0,0 +1,18 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'phones'
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropForeign(['customer_id']) 
+      table.foreign('customer_id').references('customers.id').onDelete('CASCADE')
+    })
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropForeign(['customer_id']) 
+      table.foreign('customer_id').references('customers.id')
+    })
+  }
+}
---
+++ database/migrations/1712434798219_create_sales_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'sales'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropForeign(['customer_id']) 
+      table.foreign('customer_id').references('customers.id').onDelete('CASCADE')
+    })
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropForeign(['customer_id'])
+      table.foreign('customer_id').references('customers.id')
+
+    })
+  }
+}
