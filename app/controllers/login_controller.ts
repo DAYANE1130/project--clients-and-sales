5eef995492ea660f33cc8479853b77671aa4fb0c
Dayane <dayanebmartins@gmail.com>
2024-04-06 21:04:36 -0300

    feat(Login): Criando controller Login

---
+++ app/controllers/login_controller.ts
@@ -0,0 +1,16 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class LoginController {
+
+  async create({}: HttpContext) {}
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {}
+
+  /**
+   * Show individual record
+   */
+
+}
