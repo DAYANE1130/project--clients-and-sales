492934c9d1ae028e12789f9e4731b2b959ac8458
Dayane <dayanebmartins@gmail.com>
2024-04-07 22:51:36 -0300

    feat(Login): Implementa validação dos dados de entrada da função create

--- app/controllers/login_controller.ts
+++ app/controllers/login_controller.ts
@@ -1,5 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
+import { createUserValidator } from '#validators/user_validator'
 
 export default class LoginController {
 
@@ -10,6 +11,7 @@ export default class LoginController {
    */
   async store({ request, auth }: HttpContext) {
     const { email, password } = request.all()
+    await createUserValidator.validate(request.all())
     const user = await User.verifyCredentials(email, password)
     const token = await auth.use('jwt').generate(user)
     return token
